f10567ee8402d635f34ad6a611c15ae667e7bfc7
iamdelrey <stepanton94@mail.ru>
2024-05-10 12:31:26 +0300

    penultimate actions

--- src/main/java/org/example/gallery/GalleryApp.java
+++ src/main/java/org/example/gallery/GalleryApp.java
@@ -3,3 +3,10 @@ package org.example.gallery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class GalleryApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalleryApp.class, args);
+    }
+}
